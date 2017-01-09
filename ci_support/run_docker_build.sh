2ff4baa96dfb79f6d456bc9c119c54e57dc0d332
Bas Nijholt <basnijholt@gmail.com>
2017-01-09 11:21:51 +0100

    MNT: Re-rendered with conda-smithy 1.7.0

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -49,32 +49,7 @@ source run_conda_forge_build_setup
 yum install -y devtoolset-2-gcc-gfortran
 
 
-# Embarking on 4 case(s).
-    set -x
-    export CONDA_NPY=110
-    export CONDA_PY=34
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_NPY=111
-    export CONDA_PY=34
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_NPY=110
-    export CONDA_PY=35
-    set +x
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    set -x
-    export CONDA_NPY=111
-    export CONDA_PY=35
-    set +x
+# Embarking on 1 case(s).
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 EOF
