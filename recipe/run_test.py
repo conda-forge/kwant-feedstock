97fd4f249e0b9535f6a292657b64ed14c38710f2
Anton Akhmerov <anton.akhmerov@gmail.com>
2019-03-04 05:01:51 -0500

    correctly exit with an error code

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,9 +1,12 @@
+import sys
 import os.path
+
 from pytest import main
+
 import kwant
 
 def test():
     kwant_path = os.path.dirname(os.path.abspath(kwant.__file__))
-    return main([kwant_path, "-x", "-v"])
+    sys.exit(main([kwant_path, "-x", "-v"]))
 
 test()
